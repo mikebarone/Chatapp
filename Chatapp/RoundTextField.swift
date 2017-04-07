4336f49763c030749d43b9b58b72642db2e2d24f
mikebarone <mikebarone@gmail.com>
2017-04-06 23:06:01 -0400

    Login UI

---
+++ Chatapp/RoundTextField.swift
@@ -0,0 +1,46 @@
+//
+//  RoundTextField.swift
+//  Chatapp
+//
+//  Created by Mike Barone on 2017-04-06.
+//  Copyright © 2017 Mike Barone. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundTextField: UITextField {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var bgColor: UIColor? {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+    
+    @IBInspectable var placeholderColor: UIColor? {
+        didSet {
+            let rawString = attributedPlaceholder?.string != nil ? attributedPlaceholder!.string : ""
+            let str = NSAttributedString(string: rawString, attributes: [NSForegroundColorAttributeName: placeholderColor!])
+            attributedPlaceholder = str
+        }
+    }
+}
---
+++ Chatapp/RoundedButton.swift
@@ -0,0 +1,38 @@
+//
+//  RoundedButton.swift
+//  Chatapp
+//
+//  Created by Mike Barone on 2017-04-06.
+//  Copyright © 2017 Mike Barone. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var bgColor: UIColor? {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+}
