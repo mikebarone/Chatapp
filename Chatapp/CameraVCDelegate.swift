2058533e43dd177b94412779e09534830ef819f6
mikebarone <mikebarone@gmail.com>
2017-04-06 21:02:37 -0400

    Setting up camera

---
+++ Chatapp/CameraVCDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  CameraVCDelegate.swift
+//  Chatapp
+//
+//  Created by Mike Barone on 2017-04-06.
+//  Copyright © 2017 Mike Barone. All rights reserved.
+//
+
+import Foundation
+
+protocol CameraVCDelegate {
+    func shouldEnableCameraUI(enabled: Bool)
+    func shouldEnableRecordUI(enabled: Bool)
+    func recordingHasStarted()
+    func canStartRecording()
+}
